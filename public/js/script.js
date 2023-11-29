073ec76acdb513a22cc3cca0793c89b1cd5b963e
Armaandeep Singh <136880562+armaan-deep@users.noreply.github.com>
2023-11-29 19:38:32 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -56,7 +56,7 @@
   
  let navButton=document.getElementsByClassName("navbar-toggler");
  let filter=document.getElementsByClassName("collapse"); 
-navButton.addEventListener("click",()=>{
+navButton[0].addEventListener("click",()=>{
     let switchbtn="false";
     if(switchbtn=="false"){
      filter.style.marginTop="200px";
